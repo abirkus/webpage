bb65ae4c6ccc61ca059142653354a969a747df06
Valeriia <v.braterska@gmail.com>
2022-08-26 21:56:58 +0300

    added logistic comments to order

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import Layout from '../components/Layout/Layout';
 import { Store } from '../../utils/Store';
@@ -9,14 +9,34 @@ import ServicesDataTable from 'components/Table/ServicesDataTable';
 import cartTableColumns from 'components/Table/Columns/CartServicesColumns';
 import { CardShadow } from 'components/StyledBaseComponents/CardShadow';
 import { totalPrice } from '../../utils/helperFunctions';
+import ControlledInputField from 'components/Forms/Fields/ControlledInputField';
+import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
+import { OrderDetailsType } from '../../utils/types';
 
 function CartScreen() {
   const router = useRouter();
-  const { state } = useContext(Store);
+  const { state, dispatch } = useContext(Store);
 
-  const { cartItems, carSize } = state;
+  const {
+    handleSubmit,
+    setValue,
+    control,
+    formState: { errors },
+  } = useForm();
 
-  const checkoutHandler = () => {
+  const { cartItems, carSize, shippingAddress } = state;
+
+  useEffect(() => {
+    Object.keys(shippingAddress).map((key) =>
+      setValue(key, shippingAddress[key as keyof OrderDetailsType]),
+    );
+  }, [state, setValue, shippingAddress]);
+
+  const checkoutHandler: SubmitHandler<FieldValues> = (data) => {
+    dispatch({
+      type: 'SAVE_SHIPPING_ADDRESS',
+      payload: data,
+    });
     router.push('/orderdetails');
   };
   const servicesHandler = () => {
@@ -38,53 +58,72 @@ function CartScreen() {
   return (
     <Layout title="Shopping Cart">
       <StepperComponent activeStep={0} />
+      <form onSubmit={handleSubmit(checkoutHandler)}>
+        <Grid container spacing={3} padding="20px">
+          <Grid item md={8} xs={12}>
+            <CardShadow
+              sx={{
+                borderRadius: '4px',
+              }}
+            >
+              <ServicesDataTable cartItemsArray={cartItems} columns={cartTableColumns} />
+            </CardShadow>
+            <br></br>
 
-      <Grid container spacing={3} padding="20px">
-        <Grid item md={8} xs={12}>
-          <CardShadow
-            sx={{
-              borderRadius: '4px',
-            }}
-          >
-            <ServicesDataTable cartItemsArray={cartItems} columns={cartTableColumns} />
-          </CardShadow>
-          <Button
-            onClick={servicesHandler}
-            variant="outlined"
-            color="primary"
-            fullWidth
-            sx={{ marginTop: '20px' }}
-          >
-            + Add more services
-          </Button>
+            <ControlledInputField
+              control={control}
+              errors={errors}
+              required
+              minLength={3}
+              fieldName="customerComments"
+              fieldLabel="Services Comments"
+              customRules={{
+                required: false,
+                minLength: undefined,
+              }}
+              extraProps={{
+                multiline: true,
+                minRows: 2,
+              }}
+            />
+            <Button
+              onClick={servicesHandler}
+              variant="outlined"
+              color="primary"
+              fullWidth
+              sx={{ marginTop: '20px' }}
+            >
+              + Add more services
+            </Button>
+          </Grid>
+          <Grid item md={4} xs={12}>
+            <CardShadow>
+              <List>
+                <ListItem sx={{ fontWeight: 'bold' }}>
+                  Estimated total price: ${totalPrice(cartItems, priceIndex)}
+                </ListItem>
+                <ListItem sx={{ color: 'dimgray' }}>
+                  Note: Total price and duration will vary based on the size of your vehicle
+                </ListItem>
+                <ListItem>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    sx={{
+                      fontSize: '1.1rem',
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    Check Out
+                  </Button>
+                </ListItem>
+              </List>
+            </CardShadow>
+          </Grid>
         </Grid>
-        <Grid item md={4} xs={12}>
-          <CardShadow>
-            <List>
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                Estimated total price: ${totalPrice(cartItems, priceIndex)}
-              </ListItem>
-              <ListItem sx={{ color: 'dimgray' }}>
-                Note: Total price and duration will vary based on the size of your vehicle
-              </ListItem>
-              <ListItem>
-                <Button
-                  onClick={checkoutHandler}
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  sx={{
-                    fontSize: '1.1rem',
-                    fontWeight: 'bold',
-                  }}
-                >
-                  Check Out
-                </Button>
-              </ListItem>
-            </List>
-          </CardShadow>
-        </Grid>
-      </Grid>
+      </form>
     </Layout>
   );
 }
